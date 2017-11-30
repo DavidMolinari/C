ba3d4f88a35252fe9de657a3609d36eff7c14460
David MOLINARI <david@rmolinari.net>
2017-11-30 11:34:10 +0100

    Avant functions

--- graphPlusCourtChemin.c
+++ graphPlusCourtChemin.c
@@ -1,57 +1,82 @@
 #include <stdio.h>  
 #include <stdlib.h>
-void p_p(int **matrix, int s, int *m);
-void prgrph(int **matrix);
+/*
+    Méthode pour afficher un tableau
+*/
+void afficheTab(int tab[], int taille);
+void plusPetitChemin(int **matrix, int **d, int **pred, int nbNoeuds);
+// void printTab(int *tab, int taille);
+void initMatrix(int **matrix, int taille);
 void main(){
+	int nb = 8;
+	int taille = 8;
+	int **matrix = NULL;
 
-int nb = 5;
-int marques[5] = {0};
+    // int *d = NULL;
+    int distance[] = {0,0,0,0,0,0,0,0};
+    int pred[] = {0,0,0,0,0,0,0,0};
 
-int taille = 5;
-int **matrix = NULL;
-
-matrix = (int **) malloc(taille*sizeof(int**)); // Tab of tab
-for (int i = 0; i < taille; i++)
-{
-	matrix[i] = (int *)malloc(taille*sizeof(int));
-}
-
-
-matrix[1][2] = 1;
-matrix[2][1] = 1;
-matrix[2][4] = 1;
-matrix[4][2] = 1;
-
-
-for (int i = 0; i <nb; i++){
-	for (int j = 0; j < nb; j++){
-		printf("%d\t",matrix[i][j] );
-	}
-	printf("\n");
-	}
-
-
-	prgrph(matrix);
-}
+	// int infini = -1;
+	
+	matrix = (int **) malloc(taille*sizeof(int**));
+	for (int i = 1; i < taille+1; i++) 
+		matrix[i] = (int *)malloc(taille*sizeof(int));
+	
 
 
 
-void prgrph(int **matrix){
-	int marques[5] = {0};
-	for (int i = 0; i < 5; i++)
-	{
-		p_p(matrix, i, marques);
-	}
-}
+	afficheTab(t, 8);
 
-void p_p(int **matrix, int s, int *m){
-	if(m[s] == 0){
-		m[s] = 1;
-		printf("%d\n",s+1);
-		for(int j = 0; j<5; j++){
-			if(matrix[s][j] == 1){
-				p_p(matrix, j, m); // j = Le sommet 
+	matrix[1][2] = 1;
+	matrix[1][3] = 3;
+	matrix[2][1] = 1;
+	matrix[2][6] = 4;
+	matrix[2][5] = 9;
+	matrix[2][7] = 4;
+	matrix[3][1] = 3;
+	matrix[3][4] = 10;
+	matrix[4][3] = 10;
+	matrix[4][7] = 7;
+	matrix[4][5] = 8;
+	matrix[5][4] = 8;
+	matrix[5][2] = 9;
+	matrix[6][2] = 4;
+	matrix[7][2] = 4;
+	matrix[7][4] = 7;
+
+	for (int i = 1; i <nb+1; i++){
+		for (int j = 1; j < nb+1; j++){
+			printf("%d\t",matrix[i][j] );
 		}
+		printf("\n");
 	}
-	}
+	
+}
+/*	
+*/
+void initMatrix(int **matrix, int taille){
+
+}
+/*
+    Méthode pour afficher un tableau
+*/
+void afficheTab(int tab[], int taille){
+	printf("Tableau : \n");
+    for (int i = 0; i < taille; i++)
+    {
+        printf("%u | ", tab[i]);
+    }
+    printf("\n");
+}
+
+
+void plusPetitChemin(int **matrix, int **d, int **pred, int nbNoeuds){
+
+	unsigned int infini = -1;
+
+	for (int i = 0; i < taille; i++)
+		distance[i] = infini;
+
+	for (int i = 0; i < taille; i++)
+		pred[i] = -1;
 }
