14af13e93abaa7a8dccb1ad4b03ac7c2e72fd81c
David MOLINARI <david@rmolinari.net>
2017-12-05 10:57:05 +0100

    HAUFFMEH

--- Hauffmann.c
+++ Hauffmann.c
@@ -11,51 +11,74 @@ struct Maillon
 	struct Maillon *fd;
 };
 
+struct node
+{
+	int code;
+	int occurence;
+	struct node *suivant;
+};
+
+// insert un element dans une liste chainee
+void insert(int i, int occ, struct node** tete);
+struct Maillon * constructListe(int * T);
 void afficheOccurence(int * T);
 int * histo(char * C);
-// void insereApres(int valeur, struct Maillon **ptr);
+void afficheListe(struct node* liste);
+
 
 void main(){
 	char * phrase = "Hello World !";
 	for (int i = 0; i < strlen(phrase); i++) printf("%c",phrase[i]);
-
 	int * T = (int *) malloc(256*sizeof(int));
 	T = histo(phrase);
-
 	afficheOccurence(T);
+
+	struct node *liste;
+	liste = (struct node*) malloc(sizeof(struct node));
+	for (int i = 0; i < 256 ; ++i)
+		if(T[i] > 0)
+			insert(i, T[i], &liste);
+	afficheListe(liste);
+
 }
 
+/* Affiche le nombre d'occurence par caractère dans un tableau de char */
 void afficheOccurence(int * T){
 	printf("\n");
-	for (int i = 0; i < 256; ++i) if(T[i] > 0) printf("le caractère : '%c' est present %d fois\n",i, T[i]);
+	for (int i = 0; i < 256; ++i) 
+		if(T[i] > 0) 
+			printf("le caractère : '%c' est present %d fois\n",i, T[i]);
 }
 
+/* Calcule l'histogramme d'une chaine de caractères */
 int * histo(char * C){
 	// Tableau d'int de 256 valeurs
 	int * T = (int *) malloc(256*sizeof(int));
 	// On init tout à 0
-	for (int i = 0; i < 256; ++i) T[i] = 0;
-	for (int i = 0; i < strlen(C); i++)T[C[i]]++;
+	for (int i = 0; i < 256; ++i) 
+		T[i] = 0;
+	for (int i = 0; i < strlen(C); i++)
+		T[C[i]]++;
 	return T;
 }
 
+void afficheListe(struct node* liste){
+	while(liste != NULL){
+		printf("'%c'=> %d fois \n ",liste->code, liste->occurence);
+		liste = liste->suivant;
+	}
+	printf("\n");
+}
 
-/*void insereApres(int valeur, struct Maillon **ptr){
-	struct Maillon *new;
-	new = (struct Maillon*) malloc(sizeof(struct Maillon));
-	new->valeur = valeur;
-	if(*ptr == NULL) {
-		new->prec = NULL;
-		new->suivant = NULL;
-		*ptr = new;
-	}else if((*ptr)->suivant == NULL){
-		new->suivant = NULL;
-		new->prec = *ptr;
-		(*ptr)->suivant = new;
-	}else{
-		new->suivant = (*ptr)->suivant;
-		(*ptr)->suivant = new;
-		new->suivant->prec = new;
-		new->prec = *ptr;
-	};
-};*/
+/* Construit une liste chainée à partir de l'histogramme d'un tableau de char */
+
+// insert un element dans une liste chainee
+void insert(int i, int occ, struct node** tete){
+	struct node *ptr;
+	ptr = (struct node*) malloc(sizeof(struct node));
+	ptr->suivant = *tete;
+	ptr->code = i;
+	ptr->occurence = occ;
+	*tete = ptr;
+}
+
