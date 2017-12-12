3f765df69b3b79ff49d30848c1411375d744c704
MOLINARI DAVID <david@rmolinari.net>
2017-12-12 13:45:20 +0100

    Add automate fonctionnel

--- automate.c
+++ automate.c
@@ -1,46 +1,50 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+
 
 int automeh(char* msg, int n);
 int main(int argc, char *argv[]){ 
 	char* msg = argv[1];
 	int n = atoi(argv[2]) ;
-	int mmmmmm = automeh(msg, n);
-	printf("%d\n",mmmmmm );
-	if(mmmmmm == 1) {
-		printf("Dylan++\n");
-} else "Dylan--\n";
+	printf("\nREGEX : a(a*ba(ba)*ab*)\n");
+   int mmmmmm = automeh(msg, n);
 }
-
-
 
 int automeh(char* msg, int n){
 	int etat = 1; 
 	for (int i = 0; i < n; i++)
-	{
-		printf("%c\n", msg[i]);
+   {
+      printf("Etat : %d => ",etat);
+      printf("%c\n", msg[i]);
 		switch(msg[i]) {
       		case 'a' :
-      		printf("MEH A\n");
       			if (etat == 1) etat = 2;
       			else if (etat == 2) etat = 3;
+      			else if (etat == 3) etat = 3;
+      			else if (etat == 4) etat = 5;
+      			else if (etat == 5) etat = 6;
+      			else if (etat == 3) etat = 3;
+      			else if (etat == 7) etat = 5;
       			else return false;
       			break;
       		case 'b' :
-      			if(etat == 3) etat = 4;
+      			if(etat == 2) etat = 4;
+      			else if(etat == 3) etat = 4;
+      			else if(etat == 5) etat = 4;
+      			else if(etat == 6) etat = 7;
+      			else if(etat == 7) etat = 7;
       			else return false;
          		break; 
-     		case 'c' :
-     			if(etat == 3) etat = 1;
-     			else if(etat == 4) etat = 4;
-      			else return false;
-         		break;
       		default :
          		printf("MEH\n");
       			return false;
    			}
 	}
-	if(etat == 4) return 1;
+   printf("Etat : %d => END OF THE GAME\n", etat);
+	if(etat == 2) return 1;
+	else if(etat == 6) return 1;
+	else if(etat == 7) return 1;
 	else return -1;
 }
